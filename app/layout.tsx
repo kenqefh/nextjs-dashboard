a782bee7d0edaaa86c052634b94678904867b5ef
Frank Dominguez <elvisf.dominguez@gmail.com>
2025-03-03 10:15:00 -0500

    Chapter 3 - Optimizing Fonts and Images: next/font, next/image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import "@/app/ui/global.css";
+import clsx from "clsx";
+import { inter } from "@/app/ui/fonts";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={(clsx(inter.className), "antialiased")}>{children}</body>
     </html>
   );
 }
