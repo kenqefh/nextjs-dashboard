4df277f1c75c28b08ad35909b95d08e0b05a9bd9
Frank Dominguez <elvisf.dominguez@gmail.com>
2025-03-04 17:29:22 -0500

    Chapter 13 - Handling Errors: error.tsx, notFound(), and not-found.tsx

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
@@ -6,6 +7,10 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
   const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,10 +27,15 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = Math.round(amount * 100);
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
-  INSERT INTO invoices (customer_id, amount, status, date)
-  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error("Create Invoice Error:", error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -44,11 +49,16 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
   const amountInCents = Math.round(amount * 100);
 
-  await sql`
-  UPDATE invoices
-  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-  WHERE id = ${id}
-  `;
+  try {
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+    `;
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error("Update Invoice Error:", error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
